04472321a0dd95a41b39ced077a3509e81733df1
Mark Peters <mapeters101@earthlink.net>
2025-06-01 12:05:23 -0400

    Work on lesson355 up to 6:20.

---
+++ lesson355.py
@@ -0,0 +1,31 @@
+class Kettle(object):
+
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+
+kenwood = Kettle("Kenwood", 9.99)
+print(kenwood.make)
+print(kenwood.price)
+
+kenwood.price = 12.75
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton", 14.55)
+
+print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+"""
+Class: template for creating objects.  All objects created using the same class will have the same characteristics.
+Object: an instance of a class.
+Instantiate: create an instance of a class.
+Method: a function defined in a class.
+Attribute: A variable bound to an instance of a class.
+"""
+
+
+
